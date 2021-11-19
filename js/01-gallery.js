da351026fe434e25b6afc83722eb0fe935325057
Igor Makojda <imakojda@gmail.com>
2021-11-19 22:58:25 +0200

    Task 1 done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,64 @@
+
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
 console.log(galleryItems);
+
+const gallery=document.querySelector(".gallery");
+
+const renderCard=crateItemGallery(galleryItems);
+
+gallery.insertAdjacentHTML('beforeend',renderCard)
+
+gallery.addEventListener('click',onImageClick)
+
+
+function crateItemGallery (galleryItems){
+
+    return galleryItems.map(({preview,original,description})=>{
+        return `
+        <div class="gallery__item">
+            <a class="gallery__link" href="${original}">
+                <img
+                    class="gallery__image"
+                    src="${preview}"
+                    data-source="${original}"
+                    alt="${description}"
+                />
+            </a>
+        </div>`
+    }).join("");
+
+}
+
+function onImageClick(evt){
+   evt.preventDefault(); 
+   
+  const divInGallery=evt.target.classList.contains('gallery__image')
+    
+  if (!divInGallery){
+        return;
+    }
+    const instance = basicLightbox.create(`
+    <img src="${evt.target.dataset.source}" width="800" height="600">
+`)
+
+instance.show()
+}
+
+
+document.addEventListener("keydown", closeImgEsc);
+
+function closeImgEsc(event) {
+    const visible = basicLightbox.visible();
+    
+    if (visible!==true){return}
+    else {
+        document.querySelector(".basicLightbox").classList.remove('basicLightbox--visible');
+        document.querySelector('.basicLightbox').remove();
+    }
+    
+}
+
+
+
+  
